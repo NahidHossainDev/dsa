6e1b1dc7fb5a0abf6f34405ca34f0e16a1aa2a08
NahidHossainDev <nahidhossain.dev@gmail.com>
2024-02-21 00:11:36 +0600

    array

--- DataStructure/Array.js
+++ DataStructure/Array.js
@@ -24,3 +24,51 @@ strings.splice(2, 0, "alien"); // O(n)
  * (b) Dynamic Array - dynamic length of array, means it expands length as we add new elements
  *
  */
+
+class MyArray {
+	constructor() {
+		this.length = 0;
+		this.data = {};
+	}
+
+	get(index) {
+		return this.data[index];
+	}
+
+	push(item) {
+		this.data[this.length] = item;
+		this.length++;
+	}
+
+	pop() {
+		const lastItem = this.data[this.length - 1];
+		delete this.data[this.length - 1];
+		this.length--;
+		return lastItem;
+	}
+
+	delete(index) {
+		const item = this.data[index];
+		shiftItems(index);
+		delete this.data[this.length];
+		return item;
+	}
+
+	shiftItems(index) {
+		for (let i = index; i < this.length; i++) {
+			this.data[i] = this.data[i + 1];
+		}
+	}
+}
+
+const newArray = new MyArray();
+
+newArray.push("hi");
+newArray.push("you");
+newArray.push("!");
+
+newArray.pop();
+
+newArray.delete(1);
+
+console.log(newArray);
