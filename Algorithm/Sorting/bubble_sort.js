233c9821503d652dba35b8fba551bdac35e9a766
NahidHossainDev <nahidhossain.dev@gmail.com>
2024-03-11 00:53:50 +0600

    sorting addeed

---
+++ Algorithm/Sorting/bubble_sort.js
@@ -0,0 +1,14 @@
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0, 1, 5, 63, 87, 283];
+
+function bubbleSort(array) {
+	for (let i = 0; i < array.length; i++) {
+		for (let k = 0; k < array.length; k++) {
+			if (array[k] > array[k + 1]) {
+				[array[k], array[k + 1]] = [array[k + 1], array[k]]; // swapping the elements
+			}
+		}
+	}
+}
+
+bubbleSort(numbers);
+console.log(numbers);
