9ab4c19259e94281d14712c9c175625b1be27746
NahidHossainDev <nahidhossain.dev@gmail.com>
2024-03-12 00:15:32 +0600

    selection sort done

--- Algorithm/Sorting/selection_sort.js
+++ Algorithm/Sorting/selection_sort.js
@@ -1,7 +1,20 @@
 const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 
 function selectionSort(array) {
-	//Code Here
+	const length = array.length;
+	for (let i = 0; i < length; i++) {
+		let min = i; // set current index as minimum;
+		let temp = array[i];
+
+		for (let k = i + 1; k < length; k++) {
+			if (array[k] < array[min]) {
+				min = k;
+			}
+		}
+		array[i] = array[min];
+		array[min] = temp;
+	}
+	return array;
 }
 
 console.log(selectionSort(numbers));
