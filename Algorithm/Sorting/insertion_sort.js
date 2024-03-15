7a3ddce098a4c2efabb0b673c840603c2a7d3443
NahidHossainDev <nahidhossain.dev@gmail.com>
2024-03-15 12:02:38 +0600

    merge_sort and Insertion_sort added

---
+++ Algorithm/Sorting/insertion_sort.js
@@ -0,0 +1,18 @@
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+function insertionSort(array) {
+	const length = array.length;
+	for (let i = 0; i < length; i++) {
+		if (array[i] < array[0]) array.unshift(array.splice(i, 1)[0]);
+		else {
+			for (let k = 1; k < length; k++) {
+				if (array[i] > array[k - 1] && array[i] < array[k]) {
+					array.splice(k, 0, array.splice(i, 1)[0]);
+				}
+			}
+		}
+	}
+	return array;
+}
+
+console.log(insertionSort(numbers));
